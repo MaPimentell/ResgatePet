cdae2452811b10b64091c47f1d70949ed40e873f
Matheus Pimentel <62631247+MaPimentell@users.noreply.github.com>
2024-09-01 20:16:51 -0300

    Dashboard-icon

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,17 +9,21 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    <div>
-                        <h2 class="font-semibold text-xl">Sua localização</h2>
+                    <div class="flex gap-2 items-center">
+                        <h2 class="flex font-bold text-xl">
+                            Sua localização
+                        </h2>
+                        <svg class="size-7" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier">
+                                <path d="M12 6H12.01M9 20L3 17V4L5 5M9 20L15 17M9 20V14M15 17L21 20V7L19 6M15 17V14M15 6.2C15 7.96731 13.5 9.4 12 11C10.5 9.4 9 7.96731 9 6.2C9 4.43269 10.3431 3 12 3C13.6569 3 15 4.43269 15 6.2Z" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path></g>
+                        </svg>
                     </div>
-                    <div id="map" class="mt-5 h-96 w-full"></div>
+                    <div id="map" class="mt-5 rounded-md h-96 w-full"></div>
                 </div>
             </div>
         </div>
     </div>
 
 
-    <script src="https://unpkg.com/leaflet@1.9.3/dist/leaflet.js"></script>
     <script>
         $(document).ready(function() {
              // Variáveis globais para armazenar latitude e longitude
@@ -47,25 +51,13 @@
 
                 // Adicionando uma camada de mapa
                 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                    maxZoom: 18,
                     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 }).addTo(map);
 
                 // Exemplo de como adicionar um marcador
                 var marker = L.marker([latitude, longitude]).addTo(map)
-                    .bindPopup('São Paulo')
                     .openPopup();
-
-                // Adicionando mais marcadores usando jQuery
-                var locations = [
-                    {lat: latitude, lng: longitude, name: 'Rio de Janeiro'},
-                    {lat: latitude, lng: longitude, name: 'Santos'}
-                ];
-
-
-                $.each(locations, function(index, location) {
-                    L.marker([location.lat, location.lng]).addTo(map)
-                        .bindPopup(location.name);
-                });
 
 
                 $.ajax({
@@ -80,7 +72,8 @@
                     },
                     dataType: 'json',
                     success: function(response) {
-
+                        console.log(longitude);
+                        console.log(latitude);
                     },
                     error: function(xhr, status, error) {
                         console.error(error);
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,6 +24,8 @@
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/toastify-js"></script>
         <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+        <script src="https://unpkg.com/leaflet@1.9.3/dist/leaflet.js"></script>
+
         <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
         @vite(['resources/css/app.css', 'resources/js/app.js'])
 
