e898b4d23ac095d930ff27963e49c62b845cb410
Matheus Pimentel <62631247+MaPimentell@users.noreply.github.com>
2024-10-22 20:28:40 -0300

    profile-edit-email

--- routes/web.php
+++ routes/web.php
@@ -18,14 +18,14 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/perfil', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/perfil', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/perfil', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
     Route::resource('mapa', MapaController::class);
     Route::get('/alerta/getAnimais', [AnimaisController::class, 'getAnimais'])->name('mapa.getAnimais.swal');
     Route::post('/alerta/storeAlerta', [AnimaisController::class, 'storeAlerta'])->name('mapa.storeAlerta.swal');
-    
+
     Route::get('/contato/{animal_id}', [ContatoController::class, 'view'])->name('contato');
 
     Route::get('/alertasUsuario', [AlertasController::class, 'view'])->name('alertasUsuario');
