31c061891055b6a06d5416d56ffa712d642c85e2
Matheus Pimentel <62631247+MaPimentell@users.noreply.github.com>
2024-11-05 23:01:16 -0300

    revert

--- public/images/views/components/danger-button.blade.php
+++
@@ -1,3 +0,0 @@
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-4 py-2 bg-red-600 border-2 border-red-600 rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-transparent hover:text-red-600 transition ease-in-out duration-150']) }}>
-    {{ $slot }}
-</button>
--- public/images/views/components/primary-button.blade.php
+++
@@ -1,3 +0,0 @@
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-4 py-2 bg-blue-600 border-2 border-blue-600 rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-transparent hover:text-blue-600  transition ease-in-out duration-150']) }}>
-    {{ $slot }}
-</button>
