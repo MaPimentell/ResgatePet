42a329a62d6dc7669b9bfd4d3749a337a11a51bd
Matheus Pimentel <62631247+MaPimentell@users.noreply.github.com>
2024-11-16 16:46:18 -0300

    Update forgot-password.js

--- resources/js/forgot-password.js
+++ resources/js/forgot-password.js
@@ -6,7 +6,5 @@ $('#btn-resetaSenha').on('click', function(){
     btnResetaSenha.html(`
         <div class="transition ease-in-out duration-800 spinner-border animate-spin inline-block w-4 h-4 border-2 border-t-2 border-transparent border-t-white group-hover:border-t-red-600 rounded-full mr-2"></div>
         ENVIAR LINK PARA REDEFINIR SENHA POR E-MAIL
-
     `);
-    
 });
