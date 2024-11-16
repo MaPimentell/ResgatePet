8c6928716afd78e12bee93200c8c8e9dd145e637
Matheus Pimentel <62631247+MaPimentell@users.noreply.github.com>
2024-11-16 17:02:49 -0300

    title-site

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta http-equiv="refresh" content="{{ config('session.lifetime') * 60 }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>Resgata Pet</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
