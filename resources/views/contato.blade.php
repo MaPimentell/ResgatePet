e323f147ff24149ee618b0ef0a722b385f1fcee4
Matheus Pimentel <62631247+MaPimentell@users.noreply.github.com>
2024-11-16 14:13:20 -0300

    contato-svg

--- resources/views/contato.blade.php
+++ resources/views/contato.blade.php
@@ -83,9 +83,7 @@
                                          class="object-cover w-full h-full transition-opacity duration-300 ease-in-out">
                                     <!-- Efeito de hover para ampliar imagem -->
                                     <div id="imagemAnimal" class="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 flex justify-center items-center transition-opacity duration-300 ease-in-out">
-                                        <svg class="size-10" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" stroke="#ffffff">
-                                            <path d="M7 10H10M10 10H13M10..."></path>
-                                        </svg>
+                                        <svg class="size-10" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#ffffff"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g id="Interface / Magnifying_Glass_Plus"> <path id="Vector" d="M7 10H10M10 10H13M10 10V7M10 10V13M15 15L21 21M10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10C17 13.866 13.866 17 10 17Z" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g> </g></svg>
                                     </div>
                                 </div>
                             </div>
