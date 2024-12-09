38ff15f703714ff63c0f3602d86ca0caf3e1002d
Matheus Pimentel <62631247+MaPimentell@users.noreply.github.com>
2024-12-09 19:27:08 -0300

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,7 +10,7 @@
         <img class="h-28 w-34" src="{{asset('images/logo_02.png')}}" alt="Resgata Pet">
     </div>
     <div class="flex flex-col justify-start mt-7">
-        <h2 class="md:text-2xl font-black tracking-tight">Acesse sua conta</h2>
+        <h2 class="md:text-2xl font-black tracking-tight text-gray-800">Acesse sua conta</h2>
         <span class="mt-4 text-gray-500 text-sm tracking-tight">Perdeu seu pet ou quer ajudar animais que estão perdidos? Entre ou cadastre-se em nossa plataforma e contribua para a busca!</span>
     </div>
     <div class="mt-5 mx-2">
